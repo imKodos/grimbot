844f753a201a0cff81dcb2524c21000c956d5adb
imkodos <notnek4@gmail.com>
2022-12-16 21:55:25 -0500

    new class

---
+++ src/stats/Prediction.java
@@ -0,0 +1,11 @@
+package stats;
+
+import org.json.simple.JSONObject;
+
+public class Prediction {
+    public JSONObject Prediction(int t1, int t2) {
+        JSONObject resultsJo = new JSONObject();
+
+        return resultsJo;
+    }
+}
